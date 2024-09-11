d718f127a05397d8a71ae7e66cf8818293292e8c
jaiomp <jompandey@gmail.com>
2024-09-12 03:07:35 +0530

    Adds unit test for ErrorBoundary

---
+++ src/tests/components/ErrorBoundry/ErrorBoundary.test.tsx
@@ -0,0 +1,35 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import ErrorBoundary from "../../../components/ErrorBoundry/ErrorBoundary";
+import { describe, test, expect } from "vitest";
+
+const ProblematicComponent = () => {
+  throw new Error("Test error");
+};
+
+describe("ErrorBoundary Component", () => {
+  test("renders children when there is no error", () => {
+    render(
+      <ErrorBoundary>
+        <div>Test Component</div>
+      </ErrorBoundary>
+    );
+
+    expect(screen.getByText("Test Component")).toBeInTheDocument();
+  });
+
+  test("catches errors and displays fallback UI", () => {
+    render(
+      <ErrorBoundary>
+        <ProblematicComponent />
+      </ErrorBoundary>
+    );
+
+    expect(screen.getByText("Something went wrong")).toBeInTheDocument();
+    expect(
+      screen.getByText(
+        "We're sorry, but an unexpected error occurred. Please try again later."
+      )
+    ).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: "Retry" })).toBeInTheDocument();
+  });
+});
