2d2395912885c41dabe97ac291f2e0d2b489ddcd
jaiomp <jompandey@gmail.com>
2024-09-11 19:51:17 +0530

    Adds masonary layout to the grid

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,54 +1,104 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import useInfiniteScroll from '../../hooks/useInfiniteScroll';
-import './Grid.css';
-import SearchInput from '../SearchInput/SearchInput';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import useInfiniteScroll from "../../hooks/useInfiniteScroll";
+import "./Grid.css";
 import { debounce } from '../../utils/debounce';
+import { PixabayPhoto } from "../../types";
+import SearchInput from "../SearchInput/SearchInput";
+
+function splitItemsIntoColumns(items: PixabayPhoto[], numColumns: number) {
+  const columns: PixabayPhoto[][] = Array.from(
+    { length: numColumns },
+    () => []
+  );
+
+  items.forEach((item: PixabayPhoto, index) => {
+    const columnIndex = index % numColumns;
+    columns[columnIndex].push(item as PixabayPhoto);
+  });
+
+  return columns;
+}
+
+function getNumberOfColumns() {
+  const screenWidth = window.innerWidth;
+  if (screenWidth >= 1024) {
+    return 3;
+  } else if (screenWidth >= 768) {
+    return 2;
+  } else {
+    return 1;
+  }
+}
 
 const Grid: React.FC = () => {
-    const [query, setQuery] = useState('');
-    const { photos, error, hasMore, setPage } = useInfiniteScroll(query);
+  const [numColumns, setNumColumns] = useState(getNumberOfColumns());
+  const [query, setQuery] = useState('');
+  
+  const { photos, error, hasMore, setPage } = useInfiniteScroll(query);
 
-    const handleSearch = debounce((value: string) => {
-        setQuery(value);
-        setPage(1);
-      }, 300);
+  const handleSearch = debounce((value: string) => {
+    setQuery(value);
+    setPage(1);
+  }, 300);
 
-    return (
-        <div>
-            <h1>Photo Grid</h1>
-            <SearchInput onSearch={handleSearch} />
-            <div className="grid">
-                {photos.map((photo) => {
-                    return (
-                        <div key={photo.id} className="item">
-                            <Link to={`/photo/${photo.id}`}>
-                                <picture>
-                                    <source
-                                        media="(max-width: 600px)"
-                                        srcSet={photo.previewURL}
-                                    />
-                                    <source
-                                        media="(max-width: 1024px)"
-                                        srcSet={photo.webformatURL}
-                                    />
-                                    <img
-                                        src={photo.largeImageURL}
-                                        alt={photo.tags}
-                                        loading="lazy"
-                                        style={{ maxWidth: '100%' }}
-                                    />
-                                </picture>
-                            </Link>
-                        </div>
-                    );
-                })}
+
+  useEffect(() => {
+    const handleResize = () => {
+      setNumColumns(getNumberOfColumns());
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const columns: PixabayPhoto[][] = splitItemsIntoColumns(photos, numColumns);
+
+  return (
+    <div>
+        <h1>Photo Grid</h1>
+        
+        <SearchInput onSearch={handleSearch} />
+        
+        <div className="grid grand-parent" style={{ '--columns' : numColumns}}>
+        {columns.map((column, columnIndex) => (
+            <div className="grid parent" key={columnIndex}>
+            {column.map((photo) => {
+                return (
+                <div key={photo.id} className="item">
+                    <Link to={`/photo/${photo.id}`}>
+                    <picture>
+                        <source
+                        media="(max-width: 600px)"
+                        srcSet={photo.previewURL}
+                        />
+                        <source
+                        media="(max-width: 1024px)"
+                        srcSet={photo.webformatURL}
+                        />
+                        <img
+                        src={photo.largeImageURL}
+                        alt={photo.tags}
+                        loading="lazy"
+                        style={{ maxWidth: "100%" }}
+                        />
+                    </picture>
+                    </Link>
+                </div>
+                );
+            })}
             </div>
-            {error && <p>{error}</p>}
-            {!hasMore && <p>No more photos to load</p>}
-            <div id="sentinel" />
+        ))}
         </div>
-    );
+        {error && <p>{error}</p>}
+        {!hasMore && <p>No more photos to load</p>}
+        {/* Sentinel element */}
+        <div id="sentinel" />
+    </div>
+  );
 };
 
 export default Grid;
--- src/types/index.ts
+++ src/types/index.ts
@@ -8,11 +8,27 @@ export interface Photo {
 }
 
 export interface PixabayPhoto {
-    id: string;
-    largeImageURL: string;
-    webformatURL: string;
-    previewURL: string;
+    id: number;
+    pageURL: string;
+    type: string;
     tags: string;
-    alt_description: string;
+    previewURL: string;
+    previewWidth: number;
+    previewHeight: number;
+    webformatURL: string;
+    webformatWidth: number;
+    webformatHeight: number;
+    largeImageURL: string;
+    fullHDURL: string;
+    imageURL: string;
+    imageWidth: number;
+    imageHeight: number;
+    imageSize: number;
+    views: number;
+    downloads: number;
+    likes: number;
+    comments: number;
+    user_id: number;
     user: string;
-}
+    userImageURL: string;
+  }
