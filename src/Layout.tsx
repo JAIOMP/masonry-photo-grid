fab63d62bfc539d0c26f6bbdee86e46b2b6bebb8
jompandey <jompandey@gmail.com>
2024-09-12 20:13:55 +0530

    Convert Layout component styles to CSS-in-JS using styled-components

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,12 +1,12 @@
 import { ReactNode } from 'react';
-import './Layout.css';
+import { StyledLayout } from './LayoutStyle';
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 const Layout = ({ children } : LayoutProps) => {
-  return <div className="layout">{children}</div>;
-}
+  return <StyledLayout>{children}</StyledLayout>;
+};
 
 export default Layout;
---
+++ src/LayoutStyle.tsx
@@ -0,0 +1,12 @@
+import { styled } from "styled-components";
+
+export const StyledLayout = styled.div`
+  max-width: 1320px;
+  margin: 0 auto;
+  padding: 0 20px;
+  box-sizing: border-box;
+
+  @media (max-width: 1320px) {
+    max-width: 100%;
+  }
+`;
